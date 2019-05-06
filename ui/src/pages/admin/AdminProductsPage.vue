0150b23f4673c48e69be2fccdb5bebab0f9e7ce1
ray <chsq00@163.com>
2019-05-06 13:00:04 +0800

    mac上可以调试了

--- ui/src/pages/admin/AdminProductsPage.vue
+++ ui/src/pages/admin/AdminProductsPage.vue
@@ -15,11 +15,47 @@
             <add-tag></add-tag>
           </a-col>
         </a-row>
+        <a-row class="inner-row">
+          <a-col :span="24">
+            <a-table
+              :columns="columns"
+              :rowKey="record => record.login.uuid"
+              :dataSource="data"
+              :pagination="pagination"
+              :loading="loading"
+              @change="handleTableChange"
+            >
+              <template slot="name" slot-scope="name">{{name.first}} {{name.last}}</template>
+            </a-table>
+          </a-col>
+        </a-row>
       </a-col>
     </a-row>
   </div>
 </template>
 <script>
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    sorter: true,
+    width: "20%",
+    scopedSlots: { customRender: "name" }
+  },
+  {
+    title: "Gender",
+    dataIndex: "gender",
+    filters: [
+      { text: "Male", value: "male" },
+      { text: "Female", value: "female" }
+    ],
+    width: "20%"
+  },
+  {
+    title: "Email",
+    dataIndex: "email"
+  }
+];
 const tags = [
   { id: 0, title: "New Products" },
   { id: 1, title: "Fruit" },
@@ -28,23 +64,47 @@ const tags = [
   { id: 4, title: "Commodity" }
 ];
 const dataSet = [
-  {id: 0, title: 'MI 9', status: 1},
-  {id: 1, title: 'MI 9se', status: 1},
-  {id: 2, title: 'Redmi 5', status: 0},
-  {id: 3, title: 'Mi 8', status: 1},
-  {id: 4, title: 'Mi 8se', status: 1}
+  { id: 0, title: "MI 9", status: 1 },
+  { id: 1, title: "MI 9se", status: 1 },
+  { id: 2, title: "Redmi 5", status: 0 },
+  { id: 3, title: "Mi 8", status: 1 },
+  { id: 4, title: "Mi 8se", status: 1 }
 ];
 export default {
-  data () {
+  data() {
     return {
       tags,
       tagViewModel: 0
-    }
+    };
+  },
+  mounted() {
+    this.fetch();
   },
   methods: {
-    onTagClose () {},
-    onAddTag (viewModel) {
+    onTagClose() {},
+    onAddTag(viewModel) {
       this.tagViewModel = viewModel;
+    },
+    handleTableChange(pagination, filters, sorter) {
+      console.log(pagination);
+      const pager = { ...this.pagination };
+      pager.current = pagination.current;
+      this.pagination = pager;
+      this.fetch({
+        results: pagination.pageSize,
+        page: pagination.current,
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...filters
+      });
+    },
+    fetch(params = {}) {
+      // Read total count from server
+      // pagination.total = data.totalCount;
+      pagination.total = 200;
+      this.loading = false;
+      this.data = dataSet;
+      this.pagination = pagination;
     }
   }
 };
