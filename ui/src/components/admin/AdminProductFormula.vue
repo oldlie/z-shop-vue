551dbd24951b26e50d1f57adf4a1c116702af0ad
oldlie <oldlie@github.com>
2019-12-02 19:54:57 +0800

    准备写套餐界面

---
+++ ui/src/components/admin/AdminProductFormula.vue
@@ -0,0 +1,50 @@
+<template>
+  <a-spin :spinning="loading">
+    <a-row>
+      <a-col :span="24">
+        <a-table :columns="columns" :dataSource="formarlData">
+          <span slot="action" slot-scope="text, record">
+            <a href="javascript:;">Delete</a>
+          </span>
+        </a-table>
+      </a-col>
+    </a-row>
+     <a-divider />
+     <a-row>
+         <a-col :md="12" :sm=24>
+             Add Form
+         </a-col>
+     </a-row>
+  </a-spin>
+</template>
+<script>
+export default {
+  props: {
+    id: { type: Number, default: 0 }
+  },
+  data() {
+    return {
+      loading: false,
+      formarlData: [],
+      columns: [
+        {
+          dataIndex: "title",
+          title: "套餐",
+          key: "title",
+          scopedSlots: { customRender: "title" }
+        },
+        {
+          dataIndex: "price",
+          title: "价格（元）",
+          key: "price"
+        },
+        {
+          dataIndex: "inventory",
+          title: "库存（件）",
+          key: "inventory"
+        }
+      ]
+    };
+  }
+};
+</script>
