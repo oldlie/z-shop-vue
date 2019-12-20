c79da27bb94c4fe06f5629c935ad7f531acdb4dc
oldlie <chsq00@163.com>
2019-12-20 20:04:43 +0800

    解决tag添加时显示不正确的问题

--- ui/src/components/AdminMenu.vue
+++ ui/src/components/AdminMenu.vue
@@ -14,13 +14,14 @@
       <a-menu-item key="1">Carousel Setting</a-menu-item>
       <a-menu-item key="2">New Product</a-menu-item>
       <a-menu-item key="3">Products Setting</a-menu-item>
-      <a-menu-item key="4">Aritcles Setting</a-menu-item>
+      <a-menu-item key="4">文章</a-menu-item>
     </a-sub-menu>
     <a-menu-item key="tags"><router-link to="/admin/tags">目录/标签</router-link></a-menu-item>
     <a-menu-item key="sub2"><router-link to="/admin/products">商品</router-link></a-menu-item>
+     <a-menu-item key="sub3"><router-link to="/admin/article">文章</router-link></a-menu-item>
     <a-sub-menu key="sub4">
       <span slot="title">
-        <span>Articles</span>
+        <span>文章</span>
       </span>
       <a-menu-item key="7">List</a-menu-item>
       <a-menu-item key="8">Tags</a-menu-item>
---
+++ ui/src/pages/admin/AdminArticlePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-spin :spinning="loading">
+    <div class="main-panel">
+      <a-row>
+        <a-col :sm="24" :md="8" :lg="6">
+          <admin-menu :activeIndex="'sub3'" :openKey="'sub3'"></admin-menu>
+        </a-col>
+
+        <a-col :sm="24" :md="16" :lg="18">
+          <a-row class="inner-row">
+            <a-button @click="onAddTag(1)" shape="circle" icon="plus" style="margin:5px;"></a-button>
+            <a-tag v-for="tag in tags" :key="tag.id" closable @close="onTagClose">{{tag.title}}</a-tag>
+          </a-row>
+        </a-col>
+      </a-row>
+    </div>
+  </a-spin>
+</template>
+<script>
+const tags = [
+  { id: 0, title: "New Products" },
+  { id: 1, title: "Fruit" },
+  { id: 2, title: "Aquatic Products" },
+  { id: 3, title: "Snacks" },
+  { id: 4, title: "Commodity" }
+];
+export default {
+  data() {
+    return {
+      loading: false,
+      tags
+    };
+  },
+  methods: {
+    onTagClose() {}
+  }
+};
+</script>
